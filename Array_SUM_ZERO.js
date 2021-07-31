41a2a4638c20f2ab87243540d2f4b78b9600fbf0
Akash <akashjuneja029@gmail.com>
2021-07-31 15:48:32 +0530

    first program added

--- Array_SUM_ZERO.js
+++ Array_SUM_ZERO.js
@@ -1,1 +1,13 @@
-let arr=[2,3,0,-2];
+let arr=[2,3,0,-2,-2];
+
+arr.sort();
+
+//Time Complexity in on2
+
+for(let i=0;i<arr.length;i++){
+    for(let j=i+1;j<arr.length;j++){
+        if(arr[i]+arr[j]==0){
+            console.log(arr[i],arr[j]);
+        }
+    }
+}
